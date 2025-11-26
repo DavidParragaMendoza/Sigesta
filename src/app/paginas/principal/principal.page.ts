fa8783e605e710c8b2996fb795fd84ae01aa3a59
David Parraga <davidparragamendoza@gmail.com>
2025-11-25 20:03:44 -0500

    Clase 25 de noviembre

--- src/app/paginas/principal/principal.page.ts
+++ src/app/paginas/principal/principal.page.ts
@@ -2,6 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -12,9 +13,16 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  irvermas(){
+    this.router.navigate(['/vermas'])
   }
 
 }
